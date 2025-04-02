c24db3bf296d22d8c3de0839186e1e6e1e4aabc8
Aguinaldo Américo <aguinaldoamerico2@gmail.com>
2025-04-01 21:57:58 -0300

    criando a função add nome a lista

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Challenge Amigo secreto - PT - Alura
+
+
+. app.js
+
+Foi declarado a variável "amigosList".
+Foi criado uma lista de array que recebe os nomes pelo usuário.
+Criando a função "function salvarNome"
--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosList = "";
+
+//criando a função adicionar nome a lista
+function salvarNome() {
+   amigosList = document.getElementById("nome").value;
+    if (amigos == "") {
+        alert("Inserir nome");
+    } else {
+        listaDeAmigos.push(amigos);
+        document.getElementById("nome").value = "";
+        atualizarLista();
+    }
+}
+
